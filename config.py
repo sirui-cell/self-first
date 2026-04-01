c05ba876bd41c24c725c50b80e5d50a90fc72b41
sirui-cell <yongqiqingting@gmail.com>
2026-04-01 13:40:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ API_URL = "https://servapi.dbotx.com/account/follow_trades"
 EDIT_FOLLOW_ORDER_URL = "https://api-bot-v1.dbotx.com/automation/follow_order"
 ASSET_API_URL = "https://servapi.dbotx.com/account/wallet/assets?page={page}&size=100&walletAddress={walletAddress}&chain=solana&sortBy=timestamp&minValueUsd"
 FOLLOW_ORDERS_API_URL = "https://api-bot-v1.dbotx.com/automation/follow_orders"
+wallets_URL = "https://api-bot-v1.dbotx.com/account/wallets"
 
 # Telegram Bot details
 BOT_TOKEN = "yourToken"
